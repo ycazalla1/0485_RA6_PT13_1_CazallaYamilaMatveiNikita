b40fb0d7bb2c5abb4c96a27422fcefc3101292ea
ycazalla1 <ycazaa1@gmail.com>
2025-05-07 08:55:22 +0200

    Versió inicil amb enunciat

--- src/main/java/com/mycompany/_ra6_pt13_1_cazallayamilamatveinikita/App.java
+++ src/main/java/com/mycompany/_ra6_pt13_1_cazallayamilamatveinikita/App.java
@@ -5,7 +5,11 @@
 package com.mycompany._ra6_pt13_1_cazallayamilamatveinikita;
 
 /**
- *
+ * Enunciat.
+ * En aquesta pràctica heu de cercar una API que doni una resposta en format
+ * JSON i crear un array list d’objectes del json retornat (podeu fer una crida
+ * o vàries, segons el servei web que consumiu).
+ * 
  * @author Nikita i Yamila
  */
 public class App {
