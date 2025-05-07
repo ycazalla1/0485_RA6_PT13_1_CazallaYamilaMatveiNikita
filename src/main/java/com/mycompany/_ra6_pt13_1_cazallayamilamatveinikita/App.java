9feb19209165b6e1091d0db254fab81236fad0d0
ycazalla1 <ycazaa1@gmail.com>
2025-05-07 08:50:18 +0200

    Versió incial

---
+++ src/main/java/com/mycompany/_ra6_pt13_1_cazallayamilamatveinikita/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._ra6_pt13_1_cazallayamilamatveinikita;
+
+/**
+ *
+ * @author Nikita i Yamila
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
